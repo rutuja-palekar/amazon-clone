53713f4972d98c26cfffbb304cea2103ec5eb1bc
Rutuja <rutujapalekr@gmail.com>
2023-11-23 20:47:24 +0530

    Card Payment Method Added

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -5,24 +5,56 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import LockIcon from '@mui/icons-material/Lock';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
-import { getFirestore, collection, query, getDocs, doc } from 'firebase/firestore';
+import { getFirestore, collection, query, getDocs, addDoc, serverTimestamp } from 'firebase/firestore';
 import { auth } from './firebase';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useElements, useStripe, CardNumberElement, CardExpiryElement, CardCvcElement } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import { getCartTotal } from './Reducer';
+import { useStateValue } from './StateProvider';
+
 
 function Payment() {
+  const stripe = useStripe()
+  const elements = useElements()
   const [paymentAddresses, setPaymentAddresses] = useState([]);
   const [paymentUser, setPaymentUser] = useState(null);
   const [paymentLoading, setPaymentLoading] = useState(true);
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [sectionIcons, setSectionIcons] = useState({
     deliveryAddress: 'down',
-    address: 'down',
     payment: 'down',
     items: 'down',
   });
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('creditDebitCard')
-  const [cardNo, setCardNo] = useState('')
 
+  const [error, setError] = useState(null)
+  const [disabled, setDisabled] = useState(true)
+  const [{ cart }, dispatch] = useStateValue()
+  const [succeeded, setSucceeded] = useState(false)
+  const [processing, setProcessing] = useState('')
+  const [paymentName, setPaymentName] = useState('');
+  const [cardDetailsEntered, setCardDetailsEntered] = useState(false);
+  const [nicknameEntered, setNicknameEntered] = useState(false);
+  const [addressFilled, setAddressFilled] = useState(false);
+  const [paymentMethodFilled, setPaymentMethodFilled] = useState(false);
+  const cartTotal = getCartTotal(cart).toFixed(2);
+
+
+  useEffect(() => {
+    const pageRefreshHandler = (event) => {
+      // Prompt the user if user tries to reload
+      event.preventDefault();
+      event.returnValue = '';
+    };
+
+    window.addEventListener('beforeunload', pageRefreshHandler);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('beforeunload', pageRefreshHandler);
+    };
+  }, []);
 
   useEffect(() => {
     const paymentLoadingDelay = 4000;
@@ -33,6 +65,11 @@ function Payment() {
 
       if (currentUser) {
         setPaymentUser(currentUser);
+
+        // Set the user's name as the Stripe customer's name
+        const paymentName = currentUser.displayName; // User's name fetched from firebase
+
+        setPaymentName(paymentName);
 
         const userUID = currentUser.uid;
         const addressesRef = collection(db, 'users', userUID, 'addresses');
@@ -58,7 +95,8 @@ function Payment() {
     }, paymentLoadingDelay);
   }, []);
 
-  if (paymentLoading) {
+
+  if (paymentLoading || !stripe || !elements) {
     return (
       <div className="paymentLoaderWrapper">
         <span className='paymentLoader'></span>
@@ -82,8 +120,10 @@ function Payment() {
     return pTags.join(', ');
   }
 
+
   const handleAddressSelection = (e, addressId) => {
     setSelectedAddress(addressId);
+    setAddressFilled(true);
   }
 
   const toggleSection = (section) => {
@@ -93,9 +133,10 @@ function Payment() {
     }))
   }
 
-  const addressContainers = paymentAddresses.map((address, index) => (
-    <div className="paymentViewAddressContainerWrapper">
-      <section className="paymentViewAddressContainer" key={address.id}>
+
+  const addressContainers = paymentAddresses.map((address) => (
+    <div className="paymentViewAddressContainerWrapper" key={address.id}>
+      <section className="paymentViewAddressContainer">
 
         {Object.keys(address).map((key) => {
 
@@ -124,11 +165,11 @@ function Payment() {
                     onChange={(e) => handleAddressSelection(e, address.id)} checked={isSelected}
                   />
 
-                  <p className='paymentAddress'>
+                  <div className='paymentAddress'>
                     <h5 className='paymentViewAddFullNameHeading'>{`${address[key].addFullName}`}
                     </h5>
                     {joinPText(address)}
-                  </p>
+                  </div>
 
                 </label>
               </div>
@@ -157,10 +198,158 @@ function Payment() {
     verticalAlign: 'middle',
   }
 
-  const validateCardNoHandler = (e) => {
-    setCardNo(e.target.value)
-
+  const reviewItemsArrowUpDownIconStyle = {
+    marginLeft: '33.5rem',
+    verticalAlign: 'middle',
   }
+
+  const fetchClientSecret = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/create-payment-intent', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          total: cartTotal * 100, // Convert cartTotal to paisa
+          currency: 'inr',
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data.clientSecret;
+      } else {
+        throw new Error('Failed to create Payment Intent');
+      }
+    } catch (error) {
+      console.error('Error fetching clientSecret:', error);
+      return null;
+    }
+  };
+
+  const addOrderToFirestore = async () => {
+    try {
+      const db = getFirestore();
+      const currentUser = auth.currentUser;
+
+      if (currentUser) {
+        const userUID = currentUser.uid;
+        const ordersRef = collection(db, 'users', userUID, 'orders');
+
+        // Get selected address
+        const selectedAddressData = paymentAddresses.find(address => address.id === selectedAddress);
+
+        // Get item details from the cart
+        const orderItems = cart.map(item => ({
+          title: item.title,
+          price: item.price,
+          image: item.image || item.singleImage,
+        }));
+
+        // Determine the order name (e.g., order1, order2, ...)
+        const orderNumber = await getOrderNumber(ordersRef);
+
+        // Create an order document
+        const orderData = {
+          timestamp: serverTimestamp(),
+          address: selectedAddressData,
+          items: orderItems,
+        };
+
+        // Add the order to Firestore
+        const docRef = await addDoc(ordersRef, { ...orderData, OrderNo: `${orderNumber}` });
+        console.log('Order added to Firestore:', docRef.id);
+      }
+    } catch (error) {
+      console.error('Error adding order to Firestore:', error);
+    }
+  };
+
+  // Helper function to get the next order number
+  const getOrderNumber = async (ordersRef) => {
+    const querySnapshot = await getDocs(ordersRef);
+    return querySnapshot.size + 1;
+  };
+
+
+  const cardDetailsSubmitHandler = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    try {
+      if (!nicknameEntered && !paymentName.trim()) {
+        setError('Please enter a nickname before proceeding with payment.');
+        setSucceeded(false);
+        setProcessing(false);
+        return;
+      }
+
+      const clientSecret = await fetchClientSecret();
+
+      if (clientSecret) {
+        const cardNumberElement = elements.getElement(CardNumberElement);
+        // console.log(cardNumberElement)
+
+        // Default card holder name is user's full name fetched from firebase
+        // If user changed default card name, new card holder name will be stored on stripe with Guest Tag
+        const paymentCardHolderName = setPaymentName(e.target.value) || paymentName;
+
+        // Create a payment method
+        const { paymentMethod } = await stripe.createPaymentMethod({
+          type: 'card',
+          card: cardNumberElement,
+          billing_details: {
+            name: paymentCardHolderName, // Default or entered card holder's name will be stored on stripe as customer's name
+          },
+        });
+
+        if (paymentMethod.error) {
+          // Handle the error while creating the payment method
+          setError(paymentMethod.error.message);
+          setSucceeded(false);
+        }
+        else {
+          // Use the resulting payment method object in confirmCardPayment
+          const { error } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: paymentMethod.id
+          });
+          // console.log(paymentMethod);
+
+          if (error) {
+            // Payment failed, handle the error
+            setError(error.message);
+            setSucceeded(false);
+          }
+          else {
+            // Payment successful, handle redirection
+            setError(null);
+            setSucceeded(true);
+            // console.log("Payment Successful!")
+          }
+          // Add order details to Firestore
+          await addOrderToFirestore();
+        }
+      } else {
+        console.error("Failed to obtain clientSecret. Cannot proceed with payment.");
+      }
+    } catch (error) {
+      console.error("Error confirming payment:", error);
+    }
+
+    setProcessing(false);
+
+  };
+
+  const cardHandler = (e) => {
+    // Listen for changes in the CardElement
+    // Also display any errors as the customer types their card details
+    setDisabled(e.empty)
+    setCardDetailsEntered(!e.empty);
+    setError(e.error ? e.error.message : "")
+    setPaymentMethodFilled(!e.error ? true : false)
+  }
+
 
   return (
     <div className="checkoutPaymentWrapper">
@@ -175,9 +364,11 @@ function Payment() {
 
       </header>
 
+
       <section className="selectDeliveryAddressWrapper">
 
         <span className='selectDeliverAddressHeadingNo'>1</span>
+
         <h1 className="selectDeliverAddressHeading">Select a delivery address
           {sectionIcons.deliveryAddress === 'down' ? (
             <KeyboardArrowDownIcon onClick={() => toggleSection('deliveryAddress')} style={keyboardArrowUpDownIconStyle} />
@@ -185,6 +376,7 @@ function Payment() {
             <KeyboardArrowUpIcon onClick={() => toggleSection('deliveryAddress')} style={keyboardArrowUpDownIconStyle} />
           )}
         </h1>
+
         {sectionIcons.deliveryAddress === 'down' && (
           <div className="showAddressesWrapper">
             <div className="showAddressContainer">
@@ -197,20 +389,18 @@ function Payment() {
                 <h6 className="paymentAddNewAddressNavLink">Add a new address</h6>
               </Link>
             </div>
-
-            <div className="selectAddressWrapper">
-              <button className='selectAddressBtn'>Use this address</button>
-            </div>
-
           </div>
         )}
+
         <hr className="selectDeliveryAddressFinishLine" />
+
       </section>
 
 
       <section className="selectPaymentMethodWrapper">
 
         <span className='selectPaymentMethodHeadingNo'>2</span>
+
         <h1 className="selectPaymentMethodHeading">Select a payment method
           {sectionIcons.payment === 'down' ? (
             <KeyboardArrowDownIcon onClick={() => toggleSection('payment')} style={keyboardArrowUpDownIconStyle} />
@@ -219,80 +409,113 @@ function Payment() {
           )}
         </h1>
 
+        {sectionIcons.payment === 'down' && (
+          <div className="cardDetailsWrapper">
 
-        <div className="cardDetailsWrapper">
+            <section className="cardPaymentMethod">
 
-          <section className="cardPaymentMethod">
+              <h5 className="paymentMethodHeading">
+                <input type="radio" className='radioCheckpaymentMethod' value={selectedPaymentMethod} onChange={() => setSelectedPaymentMethod('creditDebitCard')} checked={selectedPaymentMethod === 'creditDebitCard'} />
+                Credit or debit card
+              </h5>
 
-            <h5 className="paymentMethodHeading">
-              <input type="radio" className='radioCheckpaymentMethod' value={selectedPaymentMethod} onChange={() => setSelectedPaymentMethod('creditDebitCard')} checked={selectedPaymentMethod === 'creditDebitCard'} />
-              Credit or debit card
-            </h5>
+              <h4 className="cardDetailsHeading">Enter card details</h4>
 
-            <h4 className="cardDetailsHeading">Enter card details</h4>
+              <div className="enterCardDetailsWrapper">
 
-            <div className="enterCardDetailsWrapper">
+                <form className='paymentCardDetailsForm'>
+                  <label className='paymentCardNoLabel'> Card number
+                    <CardNumberElement onChange={cardHandler} />
+                  </label>
 
-              <fieldset className="enterCardDetailsErrorMsg">
-                <label htmlFor="cardNo" className="cardNoLabel">Card number</label>
-                <input type="text" className='cardNoInput' id='cardNo' onChange={validateCardNoHandler} value={cardNo} />
-                <span className="cardPaymentMethodErrorMsg"></span>
-              </fieldset>
+                  <label htmlFor='paymentCardHolderName' className="paymentCardHolderNameLabel">Nickname
+                    <br />
+                    <input type="text" className="paymentCardHolderNameInput" id='paymentCardHolderName' defaultValue={paymentName} onChange={(e) => {
+                      setPaymentName(e.target.value)
+                      setNicknameEntered(!!e.target.value.trim())
+                    }} />
+                  </label><br />
 
-              <fieldset className="enterCardDetailsErrorMsg">
-                <label htmlFor="cardHolderName" className="cardHolderNameLabel">Nickname</label>
-                <input type="text" className='cardHolderInput' id='cardHolderName' />
-                <span className="cardPaymentMethodErrorMsg"></span>
-              </fieldset>
+                  <label className='paymentCardExpireDtLabel'>Expiry date
+                    <CardExpiryElement onChange={cardHandler} />
+                  </label>
 
-              <fieldset className="enterCardDetailsErrorMsg">
-                <label htmlFor="cardHolderCVV" className="cardHolderCVVLabel">CVV</label>
-                <input type="text" className='cardCVVInput' id='cardHolderCVV' />
-                <span className="cardPaymentMethodErrorMsg"></span>
-              </fieldset>
+                  <label className='paymentCardCvcLabel'>CVC
+                    <CardCvcElement onChange={cardHandler} />
+                  </label>
 
-              <fieldset className="enterCardDetailsErrorMsg">
-                <label htmlFor="cardExpireDate" className="cardExpireDateLabel">Expiry Date</label><br />
+                  {error && <div className='paymentCardDetailsError'>{error}</div>}
 
-                <select name="cardExpireMonth" className='selectCardExpireMonth' id="cardExpireDate" >
-                  <option value="1">01</option>
-                  <option value="2">02</option>
-                  <option value="3">03</option>
-                  <option value="4">04</option>
-                  <option value="5">05</option>
-                  <option value="6">06</option>
-                  <option value="7">07</option>
-                  <option value="8">08</option>
-                  <option value="9">09</option>
-                  <option value="10">10</option>
-                  <option value="11">11</option>
-                  <option value="12">12</option>
-                </select>
-
-                <select name="cardExpireYear" className='selectCardExpireYear' id="cardExpireDate">
-                  <option value="2023">2023</option>
-                  <option value="2024">2024</option>
-                  <option value="2025">2025</option>
-                  <option value="2026">2026</option>
-                  <option value="2027">2027</option>
-                  <option value="2028">2028</option>
-                  <option value="2029">2029</option>
-                  <option value="2030">2030</option>
-                </select>
-                <span className="cardPaymentMethodErrorMsg"></span>
-              </fieldset>
-
-            </div>
-          </section>
-
-          <div className="selectPaymentMethodBtnWrapper">
-            <button className='selectPaymentMethodBtn'>Use this payment method</button>
+                </form>
+              </div>
+            </section>
           </div>
-        </div>
-
+        )}
 
         <hr className="selectPaymentMethodFinishLine" />
+
       </section>
+
+
+      <section className="paymentReviewItemsWrapper">
+
+        <span className='paymentReviewItemsHeadingNo'>3</span>
+
+        <h1 className="paymentReviewItemsHeading">Review items and delivery
+          {sectionIcons.items === 'down' ? (
+            <KeyboardArrowDownIcon onClick={() => toggleSection('items')} style={reviewItemsArrowUpDownIconStyle} />
+          ) : (
+            <KeyboardArrowUpIcon onClick={() => toggleSection('items')} style={reviewItemsArrowUpDownIconStyle} />
+          )}
+        </h1>
+
+        {sectionIcons.items === 'down' && (
+          <>
+            <div className="reviewItemsWrapper">
+              <h5 className='reviewItemsHeading'>Items dispatched by Amazon</h5>
+              {cart.map(item => (
+                <div key={item.id} className="reviewItems">
+                  {item.image &&
+                    <img className='reviewItemImg' src={item.image} alt="Product" />}
+                  {item.singleImage &&
+                    <img className='reviewItemImg' src={item.singleImage} alt="Product" />}
+                  <div className="reviewItemTitlePriceWrapper">
+                    <h5 className='reviewItemTitle'>{item.title}</h5>
+                    <h5 className='reviewItemPrice'>₹{item.price}</h5>
+                  </div>
+                </div>
+              ))}
+
+            </div>
+
+            {addressFilled && paymentMethodFilled ?
+              (<div className="placeOrderTotalWrapper">
+                <button className='placeOrderBtn' onClick={cardDetailsSubmitHandler}>
+                  <span>{processing ? <p>Processing</p> : "Place your order"}</span>
+                </button>
+                <section className="paymentPriceWrapper">
+                  <CurrencyFormat
+                    renderText={(value) => (
+                      <h3 className='paymentOrderTotal'>Order Total: {value}</h3>
+                    )}
+                    decimalScale={2}
+                    value={cartTotal}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                  />
+                  <p className='placeOrderTermsConditions'>By placing your order, you agree to Amazon's
+                    <span className="placeOrderTermsConditionsInnerTxt"> privacy notice </span>
+                    and
+                    <span className="placeOrderTermsConditionsInnerTxt"> conditions of use</span>.</p>
+                </section>
+              </div>) : null}
+          </>
+        )}
+
+        <hr className="reviewItemsPlaceOrderFinishLine" />
+
+      </section>
+
     </div>
   )
 }
